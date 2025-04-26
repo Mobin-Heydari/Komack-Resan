1f4740aaf5ad7b0fcf69c8ed995c7ed68a3e16cf
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-04-26 15:39:20 +0330

    The RecipientAddress model added

--- Backend/Server/Addresses/models.py
+++ Backend/Server/Addresses/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+
+class RecipientAddress(models.Model):
+
+    Recipient = models.ForeignKey(
+        "Users.User",
+        on_delete=models.CASCADE,
+        related_name='addresses',
+        verbose_name="خدمات گیرنده"
+    )
+
+    title = models.CharField(max_length=100, verbose_name="عنوان")
+
+    address = models.CharField(max_length=200, verbose_name="آدرس")
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="ساخته شده")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="آپدیت شده")
+
+
+    class Meta:
+        verbose_name = "آدرس مشتری"
+        verbose_name_plural = "آدرس های مشتری ها"
+
+    def __str__(self):
+        return f"{self.title} - {self.address}"
+    
