f4a4b8308ee60c8fbc9eb5d2ce8ca4c826eb8ca1
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-04-28 13:03:51 +0330

    The Service model added to the Services app .

--- Backend/Server/Services/models.py
+++ Backend/Server/Services/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from Companies.models import Company
+from Industries.models import ServiceIndustry
 
-# Create your models here.
+
+
+class Service(models.Model):
+    
+    company = models.ForeignKey(
+        Company,
+        on_delete=models.CASCADE,
+        related_name="service_compay",
+        verbose_name="شرکت"
+    )
+
+    service_industry = models.ForeignKey(
+        ServiceIndustry,
+        on_delete=models.CASCADE,
+        related_name="service",
+        verbose_name="صنعت سرویس"
+    )
+
+    title = models.CharField(max_length=255, verbose_name="عنوان")
+    slug = models.SlugField(max_length=255, verbose_name="اسلاگ")
+    descriptions = models.TextField(verbose_name="توضحات")
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="ساخته شده")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="آپدیت شده")
+
+    class Meta:
+        verbose_name = "سرویس"
+        verbose_name_plural = "سرویس ها"
+
