76d095449bcd124b3bd16e0c15f98df0d7e3fa51
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-04-28 14:23:22 +0330

    The RecepientServiceRequest mode added.

--- Backend/Server/Services/models.py
+++ Backend/Server/Services/models.py
@@ -30,4 +30,63 @@ class Service(models.Model):
     class Meta:
         verbose_name = "سرویس"
         verbose_name_plural = "سرویس ها"
+    
+    def __str__(self):
+        return self.title
 
+
+
+
+class RecepientServiceRequest(models.Model):
+
+    class PaymentStatusChoices(models.TextChoices):
+        PAIED = 'PA', 'پرداخت شده'
+        UNPAIED = 'UP', 'پرداخت نشده'
+        FAILED = 'FA', 'ناموفق'
+    
+    class ServiceStatusChoices(models.TextChoices):
+        PENDING = 'PE', 'درحال برسی'
+        IN_PROGRASS = 'IP', 'درحال اجرا'
+        FINISHED = 'FI', 'تمام شده'
+        CANCLED = 'CA', 'کنسل شده'
+        FAILED = 'FA', 'شکست خورده'
+        REPORTED = 'RE', 'گذارش شده'
+    
+
+    service = models.ForeignKey(
+        Service,
+        on_delete=models.CASCADE,
+        related_name="serivice_recepient",
+        verbose_name="سرویس"
+    )
+
+    title = models.CharField(max_length=255, verbose_name="عنوان")
+    slug = models.SlugField(max_length=255, verbose_name="اسلاگ")
+    descriptions = models.TextField(verbose_name="توضحات")
+
+    status = models.CharField(
+        max_length=3, 
+        choices=ServiceStatusChoices.choices, 
+        default=ServiceStatusChoices.PENDING, 
+        verbose_name="وضعیت کلی"
+    )
+
+    status = models.CharField(
+        max_length=3, 
+        choices=PaymentStatusChoices.choices, 
+        default=PaymentStatusChoices.UNPAIED, 
+        verbose_name="وضعیت پرداخت"
+    )
+
+    started_at = models.DateTimeField(verbose_name="شروع شده", null=True)
+    finished_at = models.DateTimeField(verbose_name="تمام شده", null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="ساخته شده")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="آپدیت شده")
+
+    class Meta:
+        verbose_name = "سرویس درخواست شده"
+        verbose_name_plural = "سرویس های درخواست شده"
+    
+    def __str__(self):
+        return self.title
