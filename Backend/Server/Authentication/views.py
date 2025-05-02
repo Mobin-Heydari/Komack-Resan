4d4ad16a6beb9eeab9abaef2c43b6951aa79d10c
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-05-02 09:01:20 +0330

    The LoginAPIView added to the views.py file.

--- Backend/Server/Authentication/views.py
+++ Backend/Server/Authentication/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.views import APIView, Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
+
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .serializers import LoginSerializer
+from .models import OneTimePassword, UserRegisterOTP
+
+from Users.models import User
+
+
+
+
+class LoginAPIView(APIView):
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+
+        if request.user.is_authenticated:
+            return Response({"message": "شما قبلاً وارد شده‌اید"}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            if serializer.is_valid():
+                phone = serializer.validated_data['phone']
+                user = User.objects.get(phone=phone)
+
+                if not user.is_active:
+                    return Response({'error': 'کاربر فعال نیست'}, status=status.HTTP_401_UNAUTHORIZED)
+                
+                refresh = RefreshToken.for_user(user)
+                
+                return Response(
+                    {
+                        'refresh': str(refresh), 
+                        'access': str(refresh.access_token)
+                    },
+                    status=status.HTTP_200_OK
+                )
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def handle_exception(self, exc):
+        if isinstance(exc, ValidationError):
+            return Response({'error': 'خطای اعتبارسنجی'}, status=status.HTTP_400_BAD_REQUEST)
+        return super().handle_exception(exc)
