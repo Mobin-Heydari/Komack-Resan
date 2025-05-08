4687832855d0e32906ad9c0630db640e40e7fb8f
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-05-08 21:02:12 +0330

    The router file is optimized and the new routes added to the CompanyRouter class.

--- Backend/Server/Companies/routers.py
+++ Backend/Server/Companies/routers.py
@@ -1,24 +1,49 @@
+from django.urls import include, path
 from rest_framework import routers
-from django.urls import path, include
-
 from . import views
 
 
 
 
 class CompanyRouter(routers.DefaultRouter):
+    """
+    Custom router for CompanyViewSet.
+    
+    Endpoints under /companies/:
+      - List:           GET /companies/
+      - Create:         POST /companies/create/
+      - Retrieve:       GET /companies/<slug:slug>/
+      - Update:         PUT/PATCH /companies/<slug:slug>/
+      - Delete:         DELETE /companies/<slug:slug>/
+      
+    You can easily extend the update route to support additional URL parameters if needed.
+    """
     def __init__(self):
         super().__init__()
-        self.register(r'companies', views.CompanyViewSet, basename='users')
+        # Register the viewset with an empty prefix so that our custom URLs under /companies/ work.
+        self.register(r'company', views.CompanyViewSet, basename='company')
 
     def get_urls(self):
         urls = super().get_urls()
         custom_urls = [
             path('', include([
+                # List route: GET /companies/
                 path('', views.CompanyViewSet.as_view({'get': 'list'})),
+                # Create route: POST /companies/create/
+                path('create/', views.CompanyViewSet.as_view({'post': 'create'})),
+                # Retrieve, update, and delete using the company slug.
                 path('<slug:slug>/', include([
-                    path('', views.CompanyViewSet.as_view({'get': 'retrieve'})),
+                    # Basic detail route: GET, PUT/PATCH, DELETE.
+                    path('', views.CompanyViewSet.as_view({
+                        'get': 'retrieve',
+                        'put': 'update',
+                        'patch': 'update',
+                        'delete': 'destroy'
+                    })),
+                    # Optionally, if ever you need a route with an extra parameter for update
+                    # path('<str:extra>/', views.CompanyViewSet.as_view({'put': 'update'})),
                 ])),
             ])),
         ]
         return urls + custom_urls
+
