ae073779c028fcd1621d8ab116afd11b15e0a51b
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-05-12 20:24:43 +0330

    The one time password login system is added and optimized.

--- Backend/Server/Authentication/urls.py
+++ Backend/Server/Authentication/urls.py
@@ -9,6 +9,12 @@ app_name = "Authentication"
 
 urlpatterns = [
     path('login/', views.LoginAPIView.as_view(), name="login"),
+
+    path('login-otp/', views.UserLoginOneTimePasswordAPIView.as_view()),
+    
+    path('login-validate-otp/<str:token>/', views.UserLoginValidateOneTimePasswordAPIView.as_view()),
+
     path('register-otp/', views.UserRegisterOtpAPIView.as_view(), name="user_register_otp"),
+
     path('validate-otp/<str:token>/', views.UserRegisterOtpValidateAPIView.as_view(), name="user_register_otp_validate"),
 ]
--- Backend/Server/Authentication/views.py
+++ Backend/Server/Authentication/views.py
@@ -7,8 +7,8 @@ from rest_framework.exceptions import ValidationError
 
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from .serializers import LoginSerializer, UserRegisterOneTimePasswordSerializer, UserRegisterSerializer
-from .models import OneTimePassword, UserRegisterOTP
+from .serializers import LoginSerializer, UserRegisterOneTimePasswordSerializer, UserRegisterSerializer, UserLoginOneTimePasswordSerializer, UserLoginValidateOneTimePasswordSerializer
+from .models import OneTimePassword, UserRegisterOTP, UserLoginOTP
 
 from Users.models import User
 
@@ -112,3 +112,64 @@ class UserRegisterOtpValidateAPIView(APIView):
                 return Response({'Detail': 'OTP does not exist'}, status=status.HTTP_404_NOT_FOUND)
         else:
             return Response({'Detail': 'You are already authenticated'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class UserLoginOneTimePasswordAPIView(APIView):
+
+    def post(self, request):
+        if not request.user.is_authenticated:
+            serializer = UserLoginOneTimePasswordSerializer(data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                otp_data = serializer.create(validated_data=serializer.validated_data)
+
+                return Response(
+                    {
+                        'Detail': {
+                            'Message': 'Otp created successfully',
+                            'token': otp_data['token'], 
+                            'code': otp_data['code']
+                        }
+                    },
+                    status=status.HTTP_201_CREATED
+                )
+            else:
+                return Response({'Detail': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'Detail': 'You are already logged in'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class UserLoginValidateOneTimePasswordAPIView(APIView):
+    def post(self, request, token):
+        otp = get_object_or_404(OneTimePassword, token=token)
+
+        login_otp = otp.login_otps.get()
+        
+        serializer = UserLoginValidateOneTimePasswordSerializer(data=request.data, context={'otp_token': token})
+
+        if request.user.is_authenticated:
+            return Response({"message": "شما قبلاً وارد شده‌اید"}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            if serializer.is_valid():
+                user = login_otp.user
+
+                if not user.is_active:
+                    return Response({'error': 'کاربر فعال نیست'}, status=status.HTTP_401_UNAUTHORIZED)
+                
+                refresh = RefreshToken.for_user(user)
+                
+                return Response(
+                    {
+                        'refresh': str(refresh), 
+                        'access': str(refresh.access_token)
+                    },
+                    status=status.HTTP_200_OK
+                )
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def handle_exception(self, exc):
+        if isinstance(exc, ValidationError):
+            return Response({'error': 'خطای اعتبارسنجی'}, status=status.HTTP_400_BAD_REQUEST)
+        return super().handle_exception(exc)
