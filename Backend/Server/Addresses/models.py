c3770a48b3c84995e3a13fb5718ec894c40c945d
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-05-12 18:21:50 +0330

    The Province and City models added and migrated.

--- Backend/Server/Addresses/models.py
+++ Backend/Server/Addresses/models.py
@@ -1,8 +1,51 @@
 from django.db import models
+from django.utils.text import slugify
+
+
+
+class Province(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="نام")
+
+    slug = models.SlugField(max_length=100, unique=True, blank=True, verbose_name="اسلاگ")
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    province = models.ForeignKey(
+        Province,
+        on_delete=models.CASCADE,
+        verbose_name="استان"
+    )
+
+    name = models.CharField(max_length=100, verbose_name="نام")
+
+    slug = models.SlugField(max_length=100, unique=True, blank=True, verbose_name="اسلاگ")
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
 
 
 
 class RecipientAddress(models.Model):
+
+    city = models.ForeignKey(
+        City,
+        on_delete=models.CASCADE,
+        verbose_name="شهر"
+    )
 
     Recipient = models.ForeignKey(
         "Users.User",
