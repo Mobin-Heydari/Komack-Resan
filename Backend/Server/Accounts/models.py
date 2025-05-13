b6ea1487138eaa665b030f797d44a6d7aa43c847
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-05-13 09:36:55 +0330

    The ResetPasswordOneTimePassword model added.

--- Backend/Server/Accounts/models.py
+++ Backend/Server/Accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from Authentication.models import OneTimePassword
+
+
+
+
+class ResetPasswordOneTimePassword(models.Model):
+    
+    otp = models.ForeignKey(
+        OneTimePassword,
+        on_delete=models.CASCADE,
+        verbose_name="کد اعتبار سنجی"
+    )
+
+    phone = models.CharField(max_length=12, verbose_name="شماره تلفن")
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="تاریخ ایجاد"
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="تاریخ بروزرسانی"
+    )
+
+
+    class Meta:
+        verbose_name = "کد اعتبار سنجی رمزعبور"
+        verbose_name_plural = "کد های اعتبار سنجی رمزعبور"
+        
+    
+    def __str__(self):
+        return f'{self.otp.token}----{self.otp.code}'
