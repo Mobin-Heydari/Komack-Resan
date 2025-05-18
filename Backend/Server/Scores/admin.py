461fd4492eb368d99d6e8765371fa88f6104dfa8
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-05-18 22:12:15 +0330

    The Scores app admin page optimized and organized.

--- Backend/Server/Scores/admin.py
+++ Backend/Server/Scores/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
 from .models import ServiceScore
 
-
 @admin.register(ServiceScore)
 class ServiceScoreAdmin(admin.ModelAdmin):
-    list_display = ('service', 'quality', 'behavior', 'time', 'overall', 'created_at')
+    list_display = (
+        'service', 
+        'quality', 
+        'behavior', 
+        'time', 
+        'created_at'
+    )
     search_fields = ('service__title',)
-    readonly_fields = ('overall', 'created_at')
+    list_filter = ('created_at',)
+    readonly_fields = ('created_at',)
+
+    fieldsets = (
+        ("اطلاعات سرویس", {
+            'fields': ('service',)
+        }),
+        ("امتیازات", {
+            'fields': ('quality', 'behavior', 'time',)
+        }),
+        ("زمان ثبت", {
+            'fields': ('created_at',)
+        }),
+    )
--- Backend/Server/Scores/apps.py
+++ Backend/Server/Scores/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ScoresConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Scores'
+    verbose_name = 'امتیازات'
