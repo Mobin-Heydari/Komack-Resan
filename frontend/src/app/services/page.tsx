e423ac1f4d64444e27feb7d12689c0dce0355635
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-05-18 17:01:54 +0330

    the services page updated.

--- frontend/src/app/services/page.tsx
+++ frontend/src/app/services/page.tsx
@@ -2,30 +2,7 @@
 
 import { useState, useEffect } from "react";
 import Link from "next/link";
-
-interface Service {
-  id: number;
-  company: string;
-  service_provider: string;
-  recipient_address: number;
-  company_card: string | null;
-  title: string;
-  slug: string;
-  descriptions: string;
-  payment_status: string;
-  payment_status_display: string;
-  service_status: string;
-  service_status_display: string;
-  is_invoiced: boolean;
-  started_at: string;
-  finished_at: string;
-  created_at: string;
-  updated_at: string;
-  overall_score: number | null;
-  time_elapsed: number;
-  payment_method: string;
-  transaction_screenshot: string;
-}
+import { Service } from "@/types/services";
 
 export default function ServicesList() {
   const [services, setServices] = useState<Service[]>([]);
